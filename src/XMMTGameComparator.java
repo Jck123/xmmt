0d4ebe265fab06be41647ce6377af436a546a9eb
Jck123 <jck.1.2.3.a@gmail.com>
2023-03-17 00:20:56 -0400

    Updated comparator, did backwards math

--- src/XMMTGameComparator.java
+++ src/XMMTGameComparator.java
@@ -2,6 +2,6 @@ import java.util.Comparator;
 
 public class XMMTGameComparator implements Comparator<XMMTGame> {
     public int compare(XMMTGame g1, XMMTGame g2) {
-        return g1.getPriorityLevel() - g2.getPriorityLevel();
+        return g2.getPriorityLevel() - g1.getPriorityLevel();
     }
 }
