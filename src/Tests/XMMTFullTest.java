175f37b757667b2d9f3d87bc04947e191b92042b
Jck123 <jck.1.2.3.a@gmail.com>
2023-05-07 00:57:28 -0400

    Created tester that runs all tests

---
+++ src/Tests/XMMTFullTest.java
@@ -0,0 +1,10 @@
+package Tests;
+
+public class XMMTFullTest {
+    public static void main(String[] args) throws InterruptedException {
+        XMMTGameTest.main(null);
+        XMMTDownloadThreadTest.main(null);
+        XMMTExtractionThreadTest.main(null);
+        XMMTThreadEngineTest.main(null);
+    }
+}
